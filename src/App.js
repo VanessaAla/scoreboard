386338c30a4295187f238277a004b07df41f3469
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-25 11:55:58 +0100

    updating files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+
 import Title from "./components/Title";
+import Player from "./components/Player";
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Player name="Vanessa" />
     </div>
   );
 }
