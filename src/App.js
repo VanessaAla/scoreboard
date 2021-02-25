37b656bb097a00b1d86479f46f691fce4ce607dd
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-02-25 12:08:25 +0100

    updating names in my components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 
 import Title from "./components/Title";
 import Player from "./components/Player";
+import Scoreboard from "./components/Scoreboard";
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <Player name="Vanessa" />
+      <Scoreboard />
     </div>
   );
 }
---
+++ src/components/Scoreboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Player from "./Player";
+
+export default function Scoreboard() {
+  return (
+    <div>
+      <Player name="Vanessa" />
+      <Player name="Theuns" />
+      <Player name="Lisa" />
+      <Player name="Billie Bob" />
+      <Player name="Vanessa" />
+    </div>
+  );
+}
